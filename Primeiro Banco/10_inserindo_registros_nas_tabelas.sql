80701d50b75e56f3a4ad73cf4e10c2ab29749466
Victoria Ferreira <victoria.senac.13@gmail.com>
2024-11-18 15:34:44 -0300

    Delete Primeiro Banco/10_inserindo_registros_nas_tabelas.sql

--- Primeiro Banco/10_inserindo_registros_nas_tabelas.sql
+++
@@ -1,9 +0,0 @@
-/* Inserindo Registros nas tabelas*/
-insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
-values (3,'Marketing');
-insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
-values (4,'Financeiro');
-insert into `bd_caso_estudo_vendas`.`tb_depto` (depto_cod,depto_desc)
-values (5,'Pessoal');
-
-select * from tb_depto
